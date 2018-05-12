e6f661794dbc4bbfd455bfcd791abd3ed63bf624
Chase Dunagan <cdunagan@utexas.edu>
2018-05-11 20:58:16 -0500

    add tests for failure to sign up correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true
+  validates :password, presence: true
   validates_confirmation_of :password
   has_secure_password
 end
--- spec/features/guest_can_sign_up_spec.rb
+++ spec/features/guest_can_sign_up_spec.rb
@@ -16,4 +16,52 @@ RSpec.describe 'Guest can sign up' do
     expect(current_path).to eq(user_page_path)
     expect(User.all.first.email).to eq("chase@gmail.com")
   end
+
+  scenario 'Guest cannot sign up if they do not give email address' do
+    visit '/'
+    click_on 'Sign Up'
+
+    expect(current_path).to eq(signup_path)
+
+    fill_in "user_email", with: ""
+    fill_in "user_username", with: "Chaser"
+    fill_in "user_password", with: "texas"
+    fill_in "user_password_confirmation", with: "texas"
+    click_on "Create User"
+
+    expect(current_path).to eq(signup_path)
+    expect(User.all.first).to eq(nil)
+  end
+
+  scenario 'Guest cannot sign up if they do not give username' do
+    visit '/'
+    click_on 'Sign Up'
+
+    expect(current_path).to eq(signup_path)
+
+    fill_in "user_email", with: "chase@gmail.com"
+    fill_in "user_username", with: ""
+    fill_in "user_password", with: "texas"
+    fill_in "user_password_confirmation", with: "texas"
+    click_on "Create User"
+
+    expect(current_path).to eq(signup_path)
+    expect(User.all.first).to eq(nil)
+  end
+
+  scenario 'Guest cannot sign up if they do not give username' do
+    visit '/'
+    click_on 'Sign Up'
+
+    expect(current_path).to eq(signup_path)
+
+    fill_in "user_email", with: "chase@gmail.com"
+    fill_in "user_username", with: ""
+    fill_in "user_password", with: "texas"
+    fill_in "user_password_confirmation", with: "texass"
+    click_on "Create User"
+
+    expect(current_path).to eq(signup_path)
+    expect(User.all.first).to eq(nil)
+  end
 end
