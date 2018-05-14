75f4bc8487ef66bb38b9ad1c2c4b773225c8a418
Chase Dunagan <cdunagan@utexas.edu>
2018-05-14 00:59:44 -0500

    add comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :password, presence: true
   validates_confirmation_of :password
+  has_many :comments
   has_secure_password
 end
