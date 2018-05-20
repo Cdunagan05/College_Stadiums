5abf2cfdd64b90b4765ecda3f17fa61c348afc20
Chase Dunagan <cdunagan@utexas.edu>
2018-05-19 19:56:27 -0500

    add test for adding comments to stadium show page

---
+++ spec/features/user_can_comment_on_stadium_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "When I go to a stadium show page" do
+  scenario "I can leave a comment on that page" do
+    stadium_1 = Areana.create(name: "Texas Stadium", capacity: 102000, state: "Texas", city: "Austin")
+    user_1 = stub_logged_in_user
+
+    visit "/areanas/#{stadium_1.id}"
+
+    expect(page).to have_content("Texas Stadium")
+
+    fill_in "comment", with: "This is a comment, I hope it works!"
+    click_on "Submit"
+
+    expect(page).to have_content("Texas Stadium")
+    expect(page).to have_content("This is a comment, I hope it works!")
+    expect(stadium_1.comments.first).to eq("This is a comment, I hope it works!")
+    expect(user_1.comments.first).to eq("This is a comment, I hope it works!")
+
+  end
+end
