8b0dd080286241b6a903850915ba85ef1801aec9
Chase Dunagan <cdunagan@utexas.edu>
2018-05-20 19:55:34 -0500

    add jqery for buttons on stadium index

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -12,10 +12,37 @@ $(document).ready(function(){
   $(".col-sm").hide();
 
   $("#Big12").click(function() {
-      $(".col-sm:contains(Texas)").toggle();
+      $(".col-sm:contains(Big 12)").toggle();
   });
   $("#ACC").click(function() {
-    $(".col-sm:contains(Miami)").toggle();
+    $(".col-sm:contains(ACC)").toggle();
+  });
+  $("#SEC").click(function() {
+      $(".col-sm:contains(SEC)").toggle();
+  });
+  $("#AAC").click(function() {
+    $(".col-sm:contains(AAC)").toggle();
+  });
+  $("#Indy").click(function() {
+      $(".col-sm:contains(Independent)").toggle();
+  });
+  $("#Pac12").click(function() {
+    $(".col-sm:contains(Pac 12)").toggle();
+  });
+  $("#Mtwest").click(function() {
+      $(".col-sm:contains(Mountain West)").toggle();
+  });
+  $("#Sunbelt").click(function() {
+    $(".col-sm:contains(Sun Belt)").toggle();
+  });
+  $("#Mac").click(function() {
+      $(".col-sm:contains(MAC)").toggle();
+  });
+  $("#CUSA").click(function() {
+    $(".col-sm:contains(Conference USA)").toggle();
+  });
+  $("#Big10").click(function() {
+    $(".col-sm:contains(Big 10)").toggle();
   });
 
 });
