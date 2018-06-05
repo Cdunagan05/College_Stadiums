fa4162f3361c83c66554de5d4e13f06ff5f2316f
Chase Dunagan <cdunagan@utexas.edu>
2018-06-05 18:23:26 -0500

    add test for user commenting only if current_user is present

--- spec/features/user_can_comment_on_stadium_spec.rb
+++ spec/features/user_can_comment_on_stadium_spec.rb
@@ -16,6 +16,19 @@ RSpec.describe "When I go to a stadium show page" do
     expect(page).to have_content("This is a comment, I hope it works!")
     expect(stadium_1.comments.first.body).to eq("This is a comment, I hope it works!")
     expect(user_1.comments.first.body).to eq("This is a comment, I hope it works!")
+  end
+
+  scenario 'I cannot comment if not signed in' do
+    stadium_1 = Areana.create(name: "Texas Stadium", capacity: 102000, state: "Texas", city: "Austin")
+
+    visit "/areanas/#{stadium_1.id}"
+
+    fill_in "Comment", with: "This is a comment, But it should not work"
+    click_on "Add Comment"
+
+    expect(page).to have_content("Signup or Login to Comment!")
+    expect(stadium_1.comments.empty?).to eq(true)
+    expect(comment.all.empty?).to eq(true)
 
   end
 end
