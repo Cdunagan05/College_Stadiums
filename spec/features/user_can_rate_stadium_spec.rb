1e1c2962b5b4fa7c564d3a103711d5523f38e3c2
Chase Dunagan <cdunagan@utexas.edu>
2018-06-08 21:13:36 -0500

    add test to rating stadium without being logged in

--- spec/features/user_can_rate_stadium_spec.rb
+++ spec/features/user_can_rate_stadium_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe 'When a user visits a stadium page' do
     expect(current_path).to eq("/areanas/#{areana_1.id}")
     expect(page).to have_content("The average rating of this stadium is 3 Stars")
   end
+
+  scenario 'cannot rate the stadium if not logged in' do
+    areana_1 = Areana.create(name: 'Darrell K Royal', capacity: 102000, state: 'Tx', city: 'Austin' )
+
+    visit "/areanas/#{areana_1.id}"
+
+    find('#rating-list').find(:xpath, 'option[3]').select_option
+    click_on "submit"
+
+    expect(current_path).to eq("/areanas/#{areana_1.id}")
+    expect(page).to have_content('The average rating of this stadium is Unrated')
+    expect(page).to have_content('Login or Sign up to rate this stadium')
+  end
 end
