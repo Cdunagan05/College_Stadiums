ddbc63b2a05af0e3872ae1051b2dd5e5d7d21499
Chase Dunagan <cdunagan@utexas.edu>
2018-06-26 19:09:53 -0500

    add test for rating ranking

--- spec/features/user_can_see_top_ranked_stadiums_spec.rb
+++ spec/features/user_can_see_top_ranked_stadiums_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe "When a user visits stadium index page" do
     areana_2 = Areana.create(name: "Texas Memorial Stadium")
     user_1 = User.create(email: "cdun@utexa.edu", username: "chase", password: "Texas")
     user_2 = User.create(email: "cdunnnnn@utexa.edu", username: "chassssse", password: "Texxxxxas")
-    rating_1 = Rating.create(user_id: 1, areana_id: 1, score: 1)
+    rating_1 = Rating.create(user_id: user_1.id, areana_id: areana_1.id, score: 5)
+    rating_1 = Rating.create(user_id: user_2.id, areana_id: areana_1.id, score: 1)
+    rating_1 = Rating.create(user_id: user_1.id, areana_id: areana_2.id, score: 4)
+    rating_1 = Rating.create(user_id: user_2.id, areana_id: areana_2.id, score: 4)
+
+    visit "/areanas"
+
+    expect(page).to have_content("1. Texas Memorial Stadium")
+    expect(page).to have_content("2. Kyle Field")
   end
 end
