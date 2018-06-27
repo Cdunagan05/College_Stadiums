cb141c5040886c0d536c07edbf99fa49e3950b61
Chase Dunagan <cdunagan@utexas.edu>
2018-06-26 22:42:10 -0500

    add ratings rankings to index page WIP

--- app/controllers/areanas_controller.rb
+++ app/controllers/areanas_controller.rb
@@ -7,6 +7,16 @@ class AreanasController < ApplicationController
 
   def index
     @areanas = Areana.all.order("name asc")
+    @areanas_ranking = Areana.find_by_sql ("
+        SELECT
+        areanas.id,
+        name,
+        AVG (score) AS average_score
+        FROM ratings
+        INNER JOIN areanas on areanas.id = ratings.areana_id
+        GROUP BY areanas.id, ratings.areana_id
+        ORDER BY average_score DESC
+      ")
   end
 
   def new
