d4a4a6a57084df2060d26aace69db1b69f689d0d
Chase Dunagan <cdunagan@utexas.edu>
2018-08-03 20:57:47 -0500

    add test for previous stadium flip

--- spec/features/user_can_flip_through_college_pages_spec.rb
+++ spec/features/user_can_flip_through_college_pages_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe "When on a stadium page" do
 
     expect(current_path).to eq("/areanas/#{areana_3.id}")
   end
+
+  scenario "I can flip to the previous stadium" do
+    areana_1 = Areana.create(name: "DKR", capacity: 102000, state: "Texas", city: "Austin" )
+    areana_2 = Areana.create(name: "The Horse Shoe", capacity: 104000, state: "Ohio", city: "Columbus" )
+    areana_3 = Areana.create(name: "The Rose Bowl", capacity: 100000, state: "California", city: "Pasadena" )
+
+    visit "/areanas/#{areana_3.id}"
+
+    click_on "Previous Stadium"
+
+    expect(current_path).to eq("/areanas/#{areana_2.id}")
+
+    click_on "Previous Stadium"
+
+    expect(current_path).to eq("/areanas/#{areana_1.id}")
+  end
 end
